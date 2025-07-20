0374c0a5db1b177b09c16a494ba89f8377f7a00e
georgiygusev <georgiy.gus@yandex.ru>
2025-07-20 19:26:46 +0300

    added cooldown between requests

--- fetch_data.py
+++ fetch_data.py
@@ -36,6 +36,6 @@ async def fetch_data(api_token: str, cards: list, ts) -> list:
                     response.raise_for_status()
 
                 result.extend(data["data"])
-                await asyncio.sleep(20)
+            await asyncio.sleep(20)
 
     return result
